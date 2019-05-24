9b4b32313452a1393b48e490ab07316de45adfae
hrocho <hrocho@vodacionline.sk>
2019-05-24 16:09:16 +0200

    py module

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # ame16
-AME16 for C++
+AME16 for C++ with optional python binding
 
 # usage
+Add .h and .cpp files into your project and:
+
 ```
-    mn = ame16::get_nuclear_mass(12,6); // nuclear mass of 12C in ame unit
-    sp = ame16::Sp(12,6); // 12C Proton Separation Energy 
-    sp = ame16::Sp(12,6,2); // 12C 2-Proton Separation Energy
+    double mn = ame16::get_nuclear_mass(12,6); // nuclear mass of 12C in ame unit
+    double sp = ame16::Sp(12,6); // 12C Proton Separation Energy 
+    double sp = ame16::Sp(12,6,2); // 12C 2-Proton Separation Energy
+```
+
+# python bindings
+Use cmake to build python module, pybind11 is required.
+```
+    import pyame
+    pyame.Sn(12,6,2)
+    pyame.Sp(12,6,2)         
 ```
---
+++ pyame.cpp
@@ -0,0 +1,28 @@
+/*
+ *  Copyright(C) 2017, Andrej Prochazka
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pybind11/pybind11.h>
+#include "ame16.h"
+
+using namespace ame16;
+namespace py = pybind11;
+
+PYBIND11_MODULE(pyame, m){
+	m.def("get_nuclear_mass",&get_nuclear_mass,"nuclear mass");
+	m.def("Sn",&Sn, "Neutron separation energy", py::arg("a"), py::arg("z"), py::arg("n")=1);
+	m.def("Sp",&Sp, "Proton separation energy",py::arg("a"), py::arg("z"), py::arg("n")=1);
+	m.def("Sa",&Sa, "Alpha separation energy");
+	m.def("BA",&BA, "Binding energy per nucleon");
+}
